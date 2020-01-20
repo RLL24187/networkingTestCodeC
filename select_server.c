60319a2acb2bddc953c638118656af3ec4faea76
rleongPd5 <rleong00@stuy.edu>
2020-01-20 16:34:34 -0500

    typo

--- select_server.c
+++ select_server.c
@@ -83,7 +83,7 @@ int main() {
 
     //if any of the pipes triggered select
     for (i = 0; i < subserver_count; i++){
-      if (FD_ISSET(pipes[i][0]), &read_fds) {
+      if (FD_ISSET(pipes[i][0], &read_fds)) {
         read(pipes[i][0], readbuffers[i], sizeof(readbuffers[i]));
         //read the data into the corresponding buffer
         printf("data received by subserver #%d: %s\n", i, readbuffers[i]);
